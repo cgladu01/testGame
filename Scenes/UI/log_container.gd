71ec638af2b4cffb437990584f2dfd6af3e35928
ccg1028 <casey.gladu@unh.edu>
2024-12-28 12:50:19 -0500

    Made Log container shrink and strectch

--- Scenes/UI/log_container.gd
+++ Scenes/UI/log_container.gd
@@ -1,6 +1,10 @@
-class log_container extends Control
+class_name  log_container extends Control
 @onready var vbox : VBoxContainer = $VBoxContainer
+@onready var button: Button = $VBoxContainer/TitleLine/Expand
 
+var drag_position = null
+var expanded : bool = false
+var SIZECHANGE : int = 400
 
 func gainHap(hap : Hap):
 	pass
@@ -10,7 +14,29 @@ func gainHap(hap : Hap):
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_expand_pressed() -> void:
+	if expanded:
+		expanded = false
+		button.text = "Expand"
+		self.size = self.size + Vector2(0, -SIZECHANGE)
+		self.position = self.position + Vector2(0, SIZECHANGE)
+	else:
+		expanded = true
+		button.text = "Minimize"
+		self.size = self.size  + Vector2(0, SIZECHANGE)
+		self.position = self.position - Vector2(0, SIZECHANGE)
+
+
+func _on_title_gui_input(event:InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			drag_position = get_global_mouse_position() - self.position
+		else:
+			drag_position = null
+	if event is InputEventMouseMotion and drag_position:
+		self.position = get_global_mouse_position() - drag_position
