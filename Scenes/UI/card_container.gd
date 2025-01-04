797b5ac6682c96456ab94878fe425035075b7ee4
ccg1028 <casey.gladu@unh.edu>
2025-01-04 14:20:00 -0500

    Created The Card visual element

---
+++ Scenes/UI/card_container.gd
@@ -0,0 +1,12 @@
+class_name CardContainer extends PanelContainer
+@onready var energyCost: Label = $VBoxContainer/HBoxContainer/EnergyCost
+@onready var cardName: Label = $VBoxContainer/HBoxContainer/CardName
+@onready var cardImage: TextureRect = $VBoxContainer/CardImage
+@onready var cardDescription: Label = $VBoxContainer/CardDescription
+
+
+var action : Action = null
+
+func setAction(action : Action):
+	self.action = action 
+
